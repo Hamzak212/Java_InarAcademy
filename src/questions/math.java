96dde361cb7bee7e36dc909234c55ca937a9d7cb
Hamza Korkmaz <Hamzakorkmaz56@gmail.com>
2022-08-21 20:05:00 -0400

    new practices

---
+++ src/questions/math.java
@@ -0,0 +1,10 @@
+public class math {
+public static void main(String[] args) {
+double radius;
+double area;
+radius = 4;
+area = radius * radius * 3.14159;
+System.out.println("The area for the circle of radius " + radius + " is " + area);
+}
+
+	}
---
+++ src/questions/q4.java
@@ -0,0 +1,6 @@
+public class q4 {
+public static void main(String[] args) {
+int i = 2, j = 2, k = 2;
+System.out.println(i + j + k);
+}
+	}

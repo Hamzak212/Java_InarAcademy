dc446d847c6b02d66353aa818800bff4a3c8b0b8
Hamza Korkmaz <Hamzakorkmaz56@gmail.com>
2022-08-25 00:04:21 -0400

    new practice

---
+++ src/questions/q10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class q10 {
+public static void main(String[] args) {
+boolean sleepIn = true;
+	if(sleepIn)
+	System.out.println("Sleep In!");
+	
+	else
+	System.out.println("Not Sleeping In");
+
+
+}
+	}
+
+
---
+++ src/questions/q11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class q11 {
+public static void main(String[] args) {
+boolean sleepIn = false;
+	if(sleepIn)
+	System.out.println("Sleep In!");
+	
+	else
+	System.out.println("Not Sleeping In");
+
+
+}
+	}
+
+

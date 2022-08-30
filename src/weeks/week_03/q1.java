53ef7d9d4a05c9c3169c7c0eb3b09fa1dd90afdc
Hamza Korkmaz <Hamzakorkmaz56@gmail.com>
2022-08-30 15:16:24 -0400

    new practice

--- src/weeks/week_03/q1.java
+++ src/weeks/week_03/q1.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+	public class q1 {
+		public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
+		System.out.print("Enter Salary Amount: ");
+		double salary = input.nextDouble();
+
+		System.out.print("Enter Years of Service: ");
+		double service = input.nextDouble();
+
+		double bonus = (((salary / 100.0) * 0.05) * 100.0);
+
+		if (service > 5){
+			System.out.println("Congrats Your Bonus is " + bonus + " !!" + "\nYour New Salary Is " + (((bonus + salary) * 100.0) / 100.0));
+		}else{
+			System.out.println("Sorry, You Are Not Eligible For a Bonus");
+		}
+
+		
+	}
+}

55d12595ddea3dae9a3b1ceadea281fa09e08981
Hamza Korkmaz <Hamzakorkmaz56@gmail.com>
2022-08-31 15:55:15 -0400

    HW Questions

--- src/weeks/week_03/q1.java
+++ src/weeks/week_03/q1.java
@@ -9,10 +9,10 @@ import java.util.Scanner;
 		System.out.print("Enter Years of Service: ");
 		double service = input.nextDouble();
 
-		double bonus = (((salary / 100.0) * 0.05) * 100.0);
+		double bonus = (((salary / 100.00) * 0.05) * 100.00);
 
 		if (service > 5){
-			System.out.println("Congrats Your Bonus is " + bonus + " !!" + "\nYour New Salary Is " + (((bonus + salary) * 100.0) / 100.0));
+			System.out.println("Congrats Your Bonus is " + ((bonus * 100.00) / 100.00) + " !!" + "\nYour New Salary Is " + (((bonus + salary) * 100.00) / 100.00));
 		}else{
 			System.out.println("Sorry, You Are Not Eligible For a Bonus");
 		}
--- src/weeks/week_03/q4.java
+++ src/weeks/week_03/q4.java
@@ -1,1 +1,12 @@
-dsdasdas
+import java.util.Scanner;
+public class q4 {
+	public static void main(String[] args) {
+		
+		int width = (int)((Math.random() * (100)) -50);
+		int height = (int)((Math.random() * (200)) -100);
+
+	
+		System.out.println("Random coordinate in rectangle centered at (0,0)");
+		System.out.println("with width 100 and height 200: (" + width + ", " + height + ")");
+	}
+}

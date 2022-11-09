8736849eb91b53af1e3007dd5a6a1f97ca7c1a16
Hamza Korkmaz <Hamzakorkmaz56@gmail.com>
2022-11-09 18:01:17 -0500

    revise

---
+++ src/chapters/chapter_eight/Exercise_08_18.java
@@ -0,0 +1,36 @@
+/**(Shuffle rows) Write a method that shuffles the rows in a two-dimensional int
+ array using the following header:
+        public static void shuffle(int[][] m)
+ Write a test program that shuffles the following matrix: */
+import java.util.*;
+public class Exercise_08_18 {
+    public static void main(String[] args) {
+
+        int[][] m = {{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}};
+
+        shuffle(m);
+
+        // Display the matrix
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print(m[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void shuffle(int[][] m){
+        int[] temp = new int[2];
+
+        for(int i = 0; i < m.length; i++){
+            int i1 = (int)(Math.random()*5);
+
+            temp[0] = m[i][0];
+            temp[1] = m[i][1];
+            m[i][0] = m[i1][0];
+            m[i][1] = m[i1][1];
+            m[i1][0] = temp[0];
+            m[i1][1] = temp[1];
+        }
+    }
+}
+
